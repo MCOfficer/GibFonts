0ffb2ccd30ebe23766cb410bb2d31d8f3e867c00
MCOfficer <mcofficer@gmx.de>
2023-09-08 12:53:10 +0200

    Rework windows building with embed-manifest, fixed ENTRYPOINT_NOT_FOUND

--- build.rs
+++ build.rs
@@ -1,23 +1,13 @@
+use embed_manifest::manifest::ExecutionLevel;
+use embed_manifest::{embed_manifest, new_manifest};
+
 fn main() {
-    if std::env::var("PROFILE").unwrap() == "release" {
-        let mut res = winres::WindowsResource::new();
-        res.set_manifest(
-            r#"
-<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-    <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-        <security>
-            <requestedPrivileges>
-                <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
-            </requestedPrivileges>
-        </security>
-    </trustInfo>
-</assembly>
-"#,
-        )
-        .set_icon("gibfonts.ico");
-        if let Err(error) = res.compile() {
-            eprintln!("{}", error);
-            std::process::exit(1);
-        }
-    }
+    embed_manifest(
+        new_manifest("GibFonts")
+            .requested_execution_level(ExecutionLevel::RequireAdministrator)
+            .ui_access(false),
+    )
+    .expect("cannot embed manifest");
+    println!("cargo:rerun-if-changed=build.rs");
+    return;
 }
